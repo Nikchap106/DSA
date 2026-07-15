772bf18df73b882032388163ab165a182d8e6c7d
Nikhil Devidas Chapolekar <chapolekarnikhil@gmail.com>
2026-07-15 13:48:53 +0530

    Add ValidPalindrome1 class to check palindromes

---
+++ Strings/ValidPalindrome1.java
@@ -0,0 +1,28 @@
+public class ValidPalindrome1 {
+
+    public static boolean isPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            if (!Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            else if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            else if (Character.toLowerCase(s.charAt(left)) !=
+                     Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            else {
+                left++;
+                right--;
+            }
+        }
+
+        return true;
+    }
+}
